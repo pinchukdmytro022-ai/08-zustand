338f630c05ca54e4b4982e4ab3774c8e6eede44b
Pinchuk Dmytro <pinchukdmitro22@gmail.com>
2026-07-26 16:49:24 +0300

    еще одно исправление

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,65 +1,59 @@
-import { QueryClient, HydrationBoundary, dehydrate,} from "@tanstack/react-query";
-import NotesClient from "./Notes.client";
-
-import {
-  TAG_OPTIONS,
-  type FilterTag,
-} from "@/lib/constants";
-
 import { fetchNotes } from "@/lib/api";
-
+import NotesClient from "./Notes.client";
+import { Tag } from "@/types/note";
+import { Metadata } from "next";
 
 type Props = {
   params: Promise<{ slug: string[] }>;
 };
 
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = await params;
+  const tag = slug[0] as Tag;
+  return {
+    title: `${tag} notes`,
+    description:
+      slug[0] === "all"
+        ? "Browse all notes available in Note Hub — your space for organized ideas, productivity boosts, and smart information capture. Discover insights across all topics."
+        : `Explore all notes tagged with "${tag}" in Note Hub — find insights, inspiration, and detailed information curated under this specific theme.`,
+    openGraph: {
+      title: `${tag} notes`,
+      description:
+        slug[0] === "all"
+          ? "All notes in Note Hub"
+          : `All notes with tag ${tag}`,
+      url:
+        slug[0] === "all"
+          ? `https://08-zustand.vercel.app/notes/filter/all`
+          : `https://08-zustand.vercel.app/notes/filter/${tag}`,
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt:
+            slug[0] === "all"
+              ? "All notes collection preview in Note Hub"
+              : `Notes tagged with ${tag} - Note Hub preview image`,
+        },
+      ],
+    },
+  };
+}
 
 export default async function Notes({ params }: Props) {
-
+  const initialQuery = "";
+  const initialPage = 1;
   const { slug } = await params;
-
-  const slugTag = slug[0];
-
-
-  const selectedTag: FilterTag | undefined =
-    slugTag === "All"
-      ? "All"
-      : TAG_OPTIONS.includes(
-          slugTag as (typeof TAG_OPTIONS)[number]
-        )
-        ? slugTag as FilterTag
-        : undefined;
-
-
-  const queryClient = new QueryClient();
-
-
-  await queryClient.prefetchQuery({
-
-    queryKey: [
-      "notes",
-      selectedTag,
-      "",
-      1,
-    ],
-
-
-    queryFn: () =>
-      fetchNotes({
-        page: 1,
-        search: "",
-        tag: selectedTag,
-      }),
-
-  });
-
-
+  const tag = slug[0] === "all" ? undefined : (slug[0] as Tag);
+  const notes = await fetchNotes(initialQuery, initialPage, tag);
 
   return (
-    <HydrationBoundary
-      state={dehydrate(queryClient)}
-    >
-      <NotesClient tag={selectedTag} />
-    </HydrationBoundary>
+    <NotesClient
+      initialQuery={initialQuery}
+      initialPage={initialPage}
+      initialTag={tag}
+      initialNotes={notes}
+    />
   );
 }
