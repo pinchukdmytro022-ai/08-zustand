7dd541f08d9d6d4dbd49b588c75ad50182d6ec49
Pinchuk Dmytro <pinchukdmitro22@gmail.com>
2026-07-26 15:38:32 +0300

    начало работы

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -1,121 +1,100 @@
-import { ErrorMessage, Field, Formik, Form, type FormikHelpers } from 'formik';
-import * as Yup from 'yup';
-import { useCallback } from 'react';
-import { useQueryClient, useMutation } from '@tanstack/react-query';
-import toast from 'react-hot-toast';
+"use client";
 
-import { createNote } from '@/lib/api';
-import {
-  TAG_OPTIONS,
-  TITLE_MIN_LENGTH,
-  TITLE_MAX_LENGTH,
-  CONTENT_MAX_LENGTH,
-  DEFAULT_TAG
-} from '@/lib/constants';
-import type { Tag } from '@/lib/constants';
-
-import css from './NoteForm.module.css';
-
-interface NoteFormValues {
-  title: string;
-  content: string;
-  tag: Tag;
-}
-
-const initialValues: NoteFormValues = {
-  title: '',
-  content: '',
-  tag: DEFAULT_TAG,
-};
+import css from "./NoteForm.module.css";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
+import { createNote } from "@/lib/api";
+import { NewNote, Tag } from "@/types/note";
+import { useRouter } from "next/navigation";
+import { useNoteDraftStore } from "@/lib/store/noteStore";
 
 interface NoteFormProps {
-  onClose: () => void;
+  tags: Tag[];
 }
 
-export default function NoteForm({ onClose }: NoteFormProps) {
+export default function NoteForm({ tags }: NoteFormProps) {
+  const router = useRouter();
   const queryClient = useQueryClient();
 
-  const mutation = useMutation({
+  const { draft, setDraft, clearDraft } = useNoteDraftStore();
+
+  const handleChange = (
+    event: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
+    setDraft({
+      ...draft,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleCancel = () => router.push("/notes/filter/all");
+
+  const { mutate } = useMutation({
     mutationFn: createNote,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['notes'] });
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+      clearDraft();
+      router.push("/notes/filter/all");
     },
   });
 
-  const handleSubmit = useCallback(
-    (values: NoteFormValues, actions: FormikHelpers<NoteFormValues>) => {
-      mutation.mutate(values, {
-        onSuccess: () => {
-          actions.resetForm();
-          toast.success('📝 Note created successfully');
-          onClose();
-        },
-      });
-    },
-    [mutation, onClose]
-  );
-
-  const Schema = Yup.object().shape({
-    title: Yup.string()
-      .min(TITLE_MIN_LENGTH, 'Title must be at least 3 characters')
-      .max(TITLE_MAX_LENGTH, 'Title is too long')
-      .required('Title is required'),
-    content: Yup.string().max(CONTENT_MAX_LENGTH, 'Content is too long'),
-    tag: Yup.string()
-      .oneOf(TAG_OPTIONS, 'Invalid category')
-      .required('Tag is required'),
-  });
+  const handleSubmit = (formData: FormData) => {
+    const values = Object.fromEntries(formData) as NewNote;
+    mutate(values);
+  };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={Schema}
-      onSubmit={handleSubmit}
-    >
-      <Form className={css.form}>
-        <div className={css.formGroup}>
-          <label htmlFor="note-title">Title</label>
-          <Field id="note-title" type="text" name="title" className={css.input} />
-          <ErrorMessage name="title" component="span" className={css.error} />
-        </div>
+    <form action={handleSubmit} className={css.form}>
+      <label className={css.formGroup}>
+        Title
+        <input
+          className={css.input}
+          type="text"
+          name="title"
+          defaultValue={draft?.title}
+          onChange={handleChange}
+        />
+      </label>
 
-        <div className={css.formGroup}>
-          <label htmlFor="note-content">Content</label>
-          <Field
-            as="textarea"
-            id="note-content"
-            name="content"
-            rows={8}
-            className={css.textarea}
-          />
-          <ErrorMessage name="content" component="span" className={css.error} />
-        </div>
+      <label className={css.formGroup}>
+        Content
+        <textarea
+          className={css.textarea}
+          name="content"
+          defaultValue={draft?.content}
+          onChange={handleChange}
+        ></textarea>
+      </label>
 
-        <div className={css.formGroup}>
-          <label htmlFor="note-tag">Tag</label>
-          <Field as="select" id="note-tag" name="tag" className={css.select}>
-  {TAG_OPTIONS.map((tag) => (
-    <option key={tag} value={tag}>
-      {tag}
-    </option>
-  ))}
-</Field>
-          <ErrorMessage name="tag" component="span" className={css.error} />
-        </div>
+      <label className={css.formGroup}>
+        Tag
+        <select
+          className={css.select}
+          name="tag"
+          defaultValue={draft?.tag}
+          onChange={handleChange}
+        >
+          {tags.map((tag) => (
+            <option value={tag} key={tag}>
+              {tag}
+            </option>
+          ))}
+        </select>
+      </label>
 
-        <div className={css.actions}>
-          <button type="button" className={css.cancelButton} onClick={onClose}>
-            Cancel
-          </button>
-          <button
-            type="submit"
-            className={css.submitButton}
-            disabled={mutation.isPending}
-          >
-            Create note
-          </button>
-        </div>
-      </Form>
-    </Formik>
+      <div className={css.actions}>
+        <button
+          type="button"
+          className={css.cancelButton}
+          onClick={handleCancel}
+        >
+          Cancel
+        </button>
+        <button type="submit" className={css.submitButton}>
+          Create note
+        </button>
+      </div>
+    </form>
   );
 }
--- types/note.ts
+++ types/note.ts
@@ -1,10 +1,16 @@
-import type { Tag } from "@/lib/constants";
-
 export interface Note {
-  id: string;
-  title: string;
-  content: string;
-  createdAt: string;
-  updatedAt: string;
-  tag: Tag;
-}
+    id: number;
+    title: string;
+    content: string;
+    tag: string;
+    createdAt: string;
+    updatedAt: string
+};
+
+export type NewNote = {
+    title: string;
+    content: string;
+    tag: 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
+};
+
+export type Tag = 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
