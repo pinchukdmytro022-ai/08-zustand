c1f847d6069d30105a69e2a30742d425c0cb682c
Pinchuk Dmytro <pinchukdmitro22@gmail.com>
2026-07-26 16:44:36 +0300

    исправления в модалке

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,39 +1,47 @@
-"use client";
+'use client';
 
-import { ReactNode, useEffect } from "react";
-import { createPortal } from "react-dom";
-import css from "./Modal.module.css";
+import css from './Modal.module.css'
+import { createPortal } from 'react-dom';
+import { useEffect } from 'react';
 
 interface ModalProps {
-  children: ReactNode;
+  children: React.ReactNode;
   onClose: () => void;
 }
 
 export default function Modal({ children, onClose }: ModalProps) {
-  useEffect(() => {
-    const originalOverflow = document.body.style.overflow;
-    document.body.style.overflow = "hidden";
+    const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
 
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === "Escape") {
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
         onClose();
       }
     };
 
-    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keydown", handleEsc);
+    document.body.style.overflow = "hidden";
 
     return () => {
-      document.body.style.overflow = originalOverflow;
-      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keydown", handleEsc);
+      document.body.style.overflow = "";
     };
   }, [onClose]);
 
-  return createPortal(
-    <div className={css.backdrop} onClick={onClose}>
-      <div className={css.modal} onClick={(event) => event.stopPropagation()}>
-        {children}
-      </div>
-    </div>,
-    document.body,
-  );
+    return createPortal(
+    <div 
+    className={css.backdrop}
+    onClick={handleBackdropClick}
+    role="dialog"
+    aria-modal="true"
+>
+  <div className={css.modal}>{children}
+  </div>
+</div>,
+document.body
+);
 }
