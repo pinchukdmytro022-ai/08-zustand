69e9924a3513ff6bafe2504f4ed4db9100f147cb
Pinchuk Dmytro <pinchukdmitro22@gmail.com>
2026-07-26 16:53:14 +0300

    фикс в layout и page

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,4 +1,4 @@
-import css from './layoutNotes.module.css'
+import css from './LayoutNotes.module.css'
 
 type Props = {
     children: React.ReactNode;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import css from "./home.module.css"
+import css from "./Home.module.css"
 
 export default function Home () {
   return <main>
